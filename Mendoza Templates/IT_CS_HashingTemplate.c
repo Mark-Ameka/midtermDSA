e7f54c56bdafc04ca415d079621a7cac9c2d82cf
Mark-Ameka <76719987+Mark-Ameka@users.noreply.github.com>
2022-03-30 21:47:54 +0800

    Create IT_CS_HashingTemplate.c

---
+++ Mendoza Templates/IT_CS_HashingTemplate.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+	Insert IT Students using Open Hash
+	Insert CS Students using Closed Hash
+*/
+
+typedef char String[30];
+typedef int Type;
+
+typedef struct{
+	Type ID;
+	String fname;
+	String lname;
+	String mname;
+	char course[4];
+	Type year;
+}StudentInfo;
+
+typedef struct node{
+	StudentInfo IT;
+	struct node* link;
+}OpenHashList, *OpenHashLink;
+
+typedef struct{
+	StudentInfo CS;
+	Type next;
+}CSDetails;
+
+typedef struct{
+	CSDetails CSInfo;
+	Type availCS;
+}ClosedHash;
+
+typedef struct{
+	OpenHashLink OH;
+	ClosedHash CH;
+}Dictionary;
+
+int main(){
+	
+	
+	return 0;
+}
